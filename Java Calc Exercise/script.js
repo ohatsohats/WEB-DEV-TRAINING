da4031c93eb6aa95bbdac903e44ba6d66a3d29ec
ohatsohats <52337292+ohatsohats@users.noreply.github.com>
2019-07-07 02:06:12 +0800

    Add files via upload

--- Java Calc Exercise/script.js
+++ Java Calc Exercise/script.js
@@ -3,33 +3,48 @@
         var y = document.getElementById('2').value;
         var sum = Number(x) + Number(y);
         
-        alert('Sum is: '+sum)
-
-
-    }
-
+        if(x=="" || y==""){
+            alert('ERROR:NO INPUT')
+            }else{
+                alert('Sum is: '+sum);
+            }
+            
+         }   
     function minus(){
         var x = document.getElementById('1').value;
         var y = document.getElementById('2').value;
         var sum = Number(x) - Number (y);
-
-        alert ('Difference is ' + sum)
-
-     
-     }
+        
+        if(x=="" || y==""){
+            alert('ERROR:NO INPUT')
+            }else{
+                alert('Difference is: '+sum);
+            }
+            
+         }   
+    
      
      function multiply(){
          var x = document.getElementById('1').value;
          var y = document.getElementById('2').value;
-         var sum = Number(x) * Number (y);
-
-         alert ('Product is ' + sum)
-      }
+         var sum = Number(x) - Number (y);
+        
+        if(x=="" || y==""){
+            alert('ERROR:NO INPUT')
+            }else{
+                alert('Qoutient is: '+sum);
+            }
+            
+         }   
       function divide(){
           var x = document.getElementById('1').value;
           var y = document.getElementById('2').value;
-          var sum = Number(x) / Number(y);
-
-          alert ('Quotient ' + sum)
-
-      }
+          var sum = Number(x) - Number (y);
+        
+          if(x=="" || y==""){
+            alert('ERROR:NO INPUT')
+            }else{
+                alert('Qoutient is: '+sum);
+            }
+            
+         }   
